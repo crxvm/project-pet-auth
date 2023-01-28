e2ca652bd93a63bb01cb7ec91dae0689393d6cf1
Arseniy Dobaev <crxxvm@gmail.com>
2023-01-28 12:17:13 +0300

    Initial commit

---
+++ project-pet-auth-srv/src/main/java/com/project_pet/auth/ProjectPetAuthApplication.java
@@ -0,0 +1,15 @@
+package com.project_pet.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ProjectPetAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPetAuthApplication.class, args);
+	}
+
+}
---
+++ project-pet-auth-srv/src/test/java/com/project_pet/auth/ProjectPetAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_pet.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPetAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
