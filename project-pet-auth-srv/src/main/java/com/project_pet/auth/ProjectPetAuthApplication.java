d80bdbde6930afa255583ce180ec24ebbe414621
Arseniy Dobaev <crxxvm@gmail.com>
2023-01-29 14:19:56 +0300

    Liquidbase

--- project-pet-auth-srv/src/main/java/com/project_pet/auth/ProjectPetAuthApplication.java
+++ project-pet-auth-srv/src/main/java/com/project_pet/auth/ProjectPetAuthApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class
+ *
+ * @author Dobaev Arseniy
+ */
 @SpringBootApplication
 @EnableAutoConfiguration
 public class ProjectPetAuthApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectPetAuthApplication.class, args);
 	}
